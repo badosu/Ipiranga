195cc8e00a20ba542a16bb7f3c56b52e56fea4a9
Amadeus Folego <amadeusfolego@gmail.com>
2013-09-03 19:02:45 -0300

    Bumps version to 0.0.4

--- lib/ipiranga/version.rb
+++ lib/ipiranga/version.rb
@@ -1,13 +1,6 @@
 module Ipiranga
-  # Major version number
-  MAJOR = 0
-  # Minor version number
-  MINOR = 0
-  # Tiny version number
-  TINY = 3
-
   # Joins the version numbers
-  VERSION = [MAJOR, MINOR, TINY].join('.')
+  VERSION = [0, 0, 4].join('.')
 
   # Returns {VERSION}
   # @return [String]
